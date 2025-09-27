4ea53d05dd68f48bbc2747deb2de74610ef495f0
Akash <73471799+AkashAkash311@users.noreply.github.com>
2025-09-27 15:28:43 +0530

    fixed this shit shadcn dialog

--- src/containers/posts/index.tsx
+++ src/containers/posts/index.tsx
@@ -16,7 +16,7 @@ export default function CreatePostButton() {
       <>
         <Button
           onClick={() => setOpen(true)}
-          className="fixed bottom-8 right-8 rounded-full p-4 shadow-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          className="fixed bottom-8 right-8 rounded-full p-4 shadow-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer"
         >
           <Plus className="h-6 w-6" />
         </Button>
