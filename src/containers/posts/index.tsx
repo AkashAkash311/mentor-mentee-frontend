9dd9077a1151fbbe2c0da7d6fc0b0189597f5e2d
Akash <73471799+AkashAkash311@users.noreply.github.com>
2025-10-04 00:46:57 +0530

    commit

--- src/containers/posts/index.tsx
+++ src/containers/posts/index.tsx
@@ -12,7 +12,7 @@ export default function CreatePostButton() {
   };
 
   return (
-    <div className="w-screen h-screen relative"> 
+    <div className=" relative"> 
       <Button
         onClick={() => setOpen(true)}
         className="absolute bottom-8 right-8 rounded-full p-4 shadow-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer"
--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -105,7 +105,7 @@
             </Sidebar>
             <main
               className={cn(
-                "flex-1 transition-all duration-300 ease-in-out p-4 ml-64"
+                "flex-1 transition-all duration-300 ease-in-out p-4 ml-64 overflow-hidden",
               )}
             >
               {children}
